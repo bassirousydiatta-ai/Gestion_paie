12b93fbcd2720dd6ad46ca8c43dd3ffdd19a307c
Bassirou Sy Diatta <bassirousydiatta@gmail.com>
2026-09-16 03:59:25 +0100

    Fix Layout import case for Netlify

--- gestion_paie_frontend/src/App.jsx
+++ gestion_paie_frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import ProtectedRoute from './components/ProtectedRoute';
-import Layout from './components/layout/Layout';
+import Layout from './components/layout/layout';
 
 import LoginPage from './pages/LoginPage';
 import DashboardPage from './pages/DashboardPage';
